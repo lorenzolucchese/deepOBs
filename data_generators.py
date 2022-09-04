1f37e4318c55328547b22c238f8c5e9570fe944c
Lorenzo <llucchese6@gmail.com>
2022-09-04 10:57:18 +0100

    data_gen vol to float32 + no cap, new scale_fn

--- data_generators.py
+++ data_generators.py
@@ -30,7 +30,7 @@ def CustomtfDataset(files,
     # methods to be used
     def scale_fn(x, y):
         if tf.keras.backend.max(x) == 0:
-            x = tf.zeros_like(x)
+            x = tf.zeros_like(x, dtype=tf.float32)
         else:
             x = x / tf.keras.backend.max(x)
         return x, y
@@ -84,8 +84,7 @@ def CustomtfDataset(files,
             D = features.shape[1]
             features = features[:, (D//2 - NF//2):(D//2 + NF//2)]
             features = np.expand_dims(features, axis=-1)
-            features[features > 65535] = 65535
-            features = tf.convert_to_tensor(features, dtype=tf.uint16)
+            features = tf.convert_to_tensor(features, dtype=tf.float32)
             
             responses = dataset['responses'][(window-1):, horizon]
 
