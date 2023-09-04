99e97de238d63374f47b01e49ac7e33f096f283a
Lorenzo Lucchese <llucchese6@gmail.com>
2023-09-04 20:57:20 +0100

    housekeeping

--- data_processing/submit_data_stats.py
+++ data_processing/submit_data_stats.py
@@ -1,4 +1,4 @@
-from data_process import dependence_responses
+from data_process import dependence_responses, percentiles_features
 from config.directories import ROOT_DIR
 import os
 import time
@@ -12,11 +12,16 @@ if __name__ == "__main__":
     processed_data_path = os.path.join(ROOT_DIR, "data", TICKER)
     stats_path = os.path.join(ROOT_DIR, "data", "stats")
     results_path = os.path.join(ROOT_DIR, "results")
+    features = ["orderbook", "orderflow", "volume"]
 
     os.makedirs(stats_path, exist_ok=True)
 
     # ============================================================================
     startTime = time.time()
+    percentiles_features(TICKER=TICKER,
+                         processed_data_path=processed_data_path,
+                         stats_path=stats_path,
+                         features=features)
     dependence_responses(TICKER = TICKER, 
                          processed_data_path = processed_data_path, 
                          results_path = results_path,
